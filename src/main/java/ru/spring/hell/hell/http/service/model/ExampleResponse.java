3d475c3cee9f4f28f752ed05085e559d91a91acc
Daniil Titov <Daniil.A.Titov@gmail.com>
2020-01-24 15:12:43 +0300

    changed naming strategy changed configuration file type

--- src/main/java/ru/spring/hell/hell/http/service/model/ExampleResponse.java
+++
@@ -1,9 +0,0 @@
-package ru.spring.hell.hell.http.service.model;
-
-import lombok.Data;
-
-@Data
-public class ExampleResponse {
-    private final String name;
-    private final String greeting;
-}
