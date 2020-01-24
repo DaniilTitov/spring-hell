521e7cd07eab5e1ee319865434e596c445f6bafe
Daniil Titov <Daniil.A.Titov@gmail.com>
2020-01-24 14:35:35 +0300

    Initial commit

---
+++ src/test/java/ru/spring/hell/hell/HellApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.hell.hell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
